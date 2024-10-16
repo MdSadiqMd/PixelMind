b12d90c1ac90869e75198393ddb03244a2a2ff3b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-16 18:45:12 +0530

    Added workers AI

---
+++ src/app/api/schema/route.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import Cloudflare from "cloudflare";
+
+export const runtime = "edge";
+
+export async function GET(request: NextRequest) {
+    const model = request.nextUrl.searchParams.get("model");
+    if (!model) return new Response("Model not specified", { status: 400 });
+
+    const env = getRequestContext().env as any;
+    const { CLOUDFLARE_ACCOUNT_ID: account_id, CLOUDFLARE_API_TOKEN } = env;
+
+    const client = new Cloudflare({
+        apiToken: CLOUDFLARE_API_TOKEN,
+    });
+
+    const schema = await client.workers.ai.models.schema.get({
+        account_id,
+        model,
+    });
+
+    return new Response(JSON.stringify(schema), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
